9fb4a9b929b5d05fb0aee3ad762678579ac0debc
j13055 <jas.blekic@gmx.de>
2024-01-04 13:54:52 +0100

    first test

--- settings.py
+++ settings.py
@@ -19,15 +19,15 @@ excalidraw_font = f'{script_path}/font/muyao-shouxie.ttf'
 if platform.system() == 'Linux':
     # BASE_PATH 是工作目录
     # BASE_PATH is the working directory
-    BASE_FOLDER = '/home/nate/soft/trilium/trilium-trans/'
+    BASE_FOLDER = '/home/nimsaj/trilium_base/'
 
     # PATCH_FOLDER 是输出补丁的目录
     # PATCH_FOLDER is the output for patch
-    PATCH_FOLDER = '/home/nate/soft/trilium/trilium-trans-patch/'
+    PATCH_FOLDER = '/home/nimsaj/trilium_patch/'
 
     # TRANS_RELEASE_FOLDER 是翻译好的客户端发布的路径
     # TRANS_RELEASE_FOLDER is the release directory for translated clients
-    TRANS_RELEASE_FOLDER = '/home/nate/soft/trilium/trilium-trans-release/'
+    TRANS_RELEASE_FOLDER = '/home/nimsaj/trilium_release/'
 else:
     # MacOS
     BASE_FOLDER = '/Users/nate/soft/trilium/trilium-trans/'
@@ -36,12 +36,12 @@ else:
 
 # release文件名后缀
 # release file name suffix
-LANG = 'cn'
+LANG = 'de'
 
 # 翻译者信息, 会在关于页面显示
 # translator info, will be in about page
-TRANSLATOR = 'Nriver'
-TRANSLATOR_URL = 'https://github.com/Nriver/trilium-translation'
+TRANSLATOR = 'j13055'
+TRANSLATOR_URL = 'https://github.com/j13055/trilium-translation-de'
 
 # 连不到GitHub需要设置代理 USE_PROXY=False 不会用代理
 # Change following proxy setting if you need proxy to connect to GitHub. set USE_PROXY=False can ignore it.
